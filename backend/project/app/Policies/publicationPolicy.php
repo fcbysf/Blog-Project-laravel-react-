b0ad1683cec66c3eb49e3ba07d8bb0602899b2f1
youssef <youssefmoussaddeq00@gmail.com>
2025-11-16 22:07:27 +0100

    sodkfs

--- backend/project/app/Policies/publicationPolicy.php
+++ backend/project/app/Policies/publicationPolicy.php
@@ -5,6 +5,7 @@ namespace App\Policies;
 use App\Models\User;
 use App\Models\publication;
 use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Auth;
 
 class publicationPolicy
 {
@@ -45,7 +46,7 @@ class publicationPolicy
      */
     public function delete(User $user, publication $publication): bool
     {
-        return $user->id === $publication->user_id;
+        return Auth::user()->id === $publication->user_id;
     }
 
     /**
